f7b400512bccd7df1d25145849b75785f02662c4
GioLorenzo <gio.lorenzo.lui@gmail.com>
2020-11-22 19:56:15 -0500

    creating header in JS

---
+++ app.js
@@ -0,0 +1,6 @@
+let header = document.createElement('header');
+let header_img = document.createElement('img');
+header_img.setAttribute('class', 'header-img');
+header_img.setAttribute('src', './header.jpg');
+header.appendChild(header_img);
+document.body.appendChild(header);
