f6f9d102368c363808ddc50fecc8087eb3fe7be8
GioLorenzo <gio.lorenzo.lui@gmail.com>
2020-11-22 19:57:24 -0500

    finished body in JS and inserted in html

--- app.js
+++ app.js
@@ -13,3 +13,37 @@ intro_title.setAttribute('class', 'intro-title');
 
 let h1 = document.createElement('h1');
 h1.innerText = 'Effect   Appear!';
+
+let intro_content = document.createElement('div');
+intro_content.setAttribute('class', 'intro-content');
+
+let intro_text = document.createElement('div');
+intro_text.setAttribute('class', 'intro-text');
+
+let h4 = document.createElement('h4');
+h4.innerText = 'Take Amazing Shots'
+let p = document.createElement('p');
+p.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam consequuntur facere quae, iste est quo, et, nam qui quasi ducimus delectus itaque modi nesciunt alias similique voluptates ab totam laudantium deserunt? Explicabo quam at hic vitae distinctio molestiae saepe ducimus.'
+
+let intro_img = document.createElement('img');
+intro_img.setAttribute('class', 'intro-img');
+intro_img.setAttribute('src', './intro.jpg');
+
+let empty = document.createElement('div')
+empty.setAttribute('class', 'empty');
+
+intro_title.appendChild(h1);
+intro.appendChild(intro_title);
+
+intro_text.appendChild(h4);
+intro_text.appendChild(p);
+
+intro_content.appendChild(intro_text);
+intro_content.appendChild(intro_img);
+
+
+intro.appendChild(intro_title);
+intro.appendChild(intro_content);
+
+document.body.appendChild(intro);
+document.body.appendChild(empty);
