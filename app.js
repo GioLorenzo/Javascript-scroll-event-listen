6c4a68ab3bd7d55a29de1b052c52b536163a3d2e
GioLorenzo <gio.lorenzo.lui@gmail.com>
2020-11-22 19:56:48 -0500

    created title in javascript

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ header_img.setAttribute('class', 'header-img');
 header_img.setAttribute('src', './header.jpg');
 header.appendChild(header_img);
 document.body.appendChild(header);
+
+let intro = document.createElement('div');
+intro.setAttribute('class', 'intro');
+
+let intro_title = document.createElement('div');
+intro_title.setAttribute('class', 'intro-title');
+
+let h1 = document.createElement('h1');
+h1.innerText = 'Effect   Appear!';
